7f7924631f9fb16ca9fab5fdc094917598192331
“dsampah” <sampahdavid@yahoo.com>
2020-08-30 20:02:24 +0000

    check login

--- view/dashboard.php
+++ view/dashboard.php
@@ -4,10 +4,20 @@ include_once "header.php";
 include_once "attendance_menu.php";
 include_once "../utility/html_tag_class.php";
 include_once "../classes/user_class.php";
+
+// check_login();
+
+// $userpm = get_user_permission();
+// //1 faculty, 2 FI and 3 student
+
+// $get_user_id = get_user_id();
+
+
 $curUser = "Not Logged In";
 if (isset($_SESSION) and isset($_SESSION["user_id"])) {
   $curUser = user_class::create_from_session($_SESSION);
 }
+
 echoHeader("Dashboard");
 check_login();
 echo <<<__BODY_START
